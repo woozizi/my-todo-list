1c17e712c891d9e7aca7fc03595e60405047e35b
woozizi <c980805@naver.com>
2025-06-12 04:58:16 +0900

    ♻️ refactor : prefetch 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,24 @@
+import { fetchTodos } from '@/apis/todo-api';
 import TodoContainer from '@/components/todo-container';
+import {
+  dehydrate,
+  HydrationBoundary,
+  QueryClient,
+} from '@tanstack/react-query';
 
-export default function Home() {
+export default async function Home() {
+  const qc = new QueryClient();
+  await qc.prefetchQuery({
+    queryKey: ['todos'],
+    queryFn: () => fetchTodos(),
+  });
+
   return (
-    <div className='mx-auto flex max-w-xl flex-col p-4'>
-      <h1 className='mb-4 text-2xl font-bold'>TODO LIST</h1>
-      <TodoContainer />
-    </div>
+    <HydrationBoundary state={dehydrate(qc)}>
+      <div className='mx-auto flex max-w-xl flex-col p-4'>
+        <h1 className='mb-4 text-2xl font-bold'>TODO LIST</h1>
+        <TodoContainer />
+      </div>
+    </HydrationBoundary>
   );
 }
