5bd07d6b5be55a20e7c4969561402da1410ade2c
woozizi <c980805@naver.com>
2025-06-12 02:16:59 +0900

    🩹 fix : 기본 UI 및 todo 조회

--- src/hooks/use-todo.ts
+++ src/hooks/use-todo.ts
@@ -3,11 +3,12 @@ import { Todo } from '@/types/todo';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 // 전체 todo 조회 hook
-export const useTodos = () =>
-  useQuery<Todo[]>({
+export const useTodos = () => {
+  return useQuery<Todo[]>({
     queryKey: ['todos'],
     queryFn: fetchTodos,
   });
+};
 
 // todo 생성 hook
 export const useAddTodo = () => {
