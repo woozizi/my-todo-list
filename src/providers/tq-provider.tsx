d5c07b7565e3ffa898763c9446134d345fed99b2
woozizi <c980805@naver.com>
2025-06-12 22:34:18 +0900

    ♻️ refactor : staleTime 수정

--- src/providers/tq-provider.tsx
+++ src/providers/tq-provider.tsx
@@ -10,7 +10,7 @@ function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 60 * 1000,
+        staleTime: 5 * 1000,
       },
     },
   });
